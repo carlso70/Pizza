a53050e1261a931c4b310476cacad7811422daf0
James Carlson <carlso70@purdue.edu>
2017-12-04 20:32:26 -0500

    Add cors

--- backend/main.go
+++ backend/main.go
@@ -6,13 +6,20 @@ import (
 	"net/http"
 
 	"github.com/carlso70/pizza/backend/routing"
+	"github.com/gorilla/handlers"
 )
 
 func main() {
 	fmt.Println("Launching Pizza Server")
 
+	headersOk := handlers.AllowedHeaders([]string{"X-Requested-With"})
+	originsOk := handlers.AllowedOrigins([]string{os.Getenv("ORIGIN_ALLOWED")})
+	methodsOk := handlers.AllowedMethods([]string{"GET", "HEAD", "POST", "PUT", "OPTIONS"})
+
 	// GetInstance inits the gamemanager singleton
 	router := routing.NewRouter()
 
-	log.Fatal(http.ListenAndServe(":8080", router))
+	// start server listen
+	// with error handling
+	log.Fatal(http.ListenAndServe(":8080", handlers.CORS(originsOk, headersOk, methodsOk)(router)))
 }
